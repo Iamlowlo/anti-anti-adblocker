9328da9f00534c80fef7f7a316b9ff8a14218772
IamLoWLo <iamlowlo@gmail.com>
2021-06-01 13:16:08 +0200

    feat(Recursiveness): add logic to recursive rety removing blocker before page load for slow websites

--- removeAntiAdBlocker.js
+++ removeAntiAdBlocker.js
@@ -1,10 +1,30 @@
-window.addEventListener('load', function() {
-  console.log('Anti Anti AdBlocker')
-  for (const fcAbRoot of document.querySelectorAll('.fc-ab-root').values()) {
-    fcAbRoot.remove()
-  }
+const removeOverflow = () => new Promise(resolve => {
   setTimeout(() => {
+    let removed = false
+    for (const fcAbRoot of document.querySelectorAll('.fc-ab-root').values()) {
+      fcAbRoot.remove()
+      removed = true
+    }
+
     const body = document.getElementsByTagName('body')[0]
     body.style.overflow = 'initial'
+    resolve(removed)
   }, 5)
+})
+
+let retryCount = 6
+
+const recursiveRetry = async () => {
+  const hasBeenRemoved = await removeOverflow()
+  retryCount = hasBeenRemoved ? 0 : retryCount - 1
+  if (retryCount) {
+    await recursiveRetry()
+  }
+}
+
+console.log('-- Anti Anti AdBlocker v2.0 --')
+recursiveRetry().then()
+
+window.addEventListener('load', async function() {
+    await removeOverflow()
 })
